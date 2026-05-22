51999faa64134e35558542c541646620ab653dd1
EstebanHuriot <esteban.martin@bsb-education.com>
2026-05-22 18:30:50 +0200

    Add LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -3,10 +3,17 @@ import numpy as np
 
 class LinearRegression:
     
-    def __init__(self, solver, learning_rate, n_iterations):
+    VALID_SOLVERS = {"ne", "bgd", "sgd"}
+
+    def __init__(self, solver, learning_rate=None, n_iterations=None, epochs=None):
+        
+        if solver not in self.VALID_SOLVERS:
+            raise ValueError(f'{solver} not accepted, solver must be within {self.VALID_SOLVERS}')
+        
         self.solver = solver
         self.learning_rate = learning_rate
         self.n_iterations = n_iterations
+        self.epochs = epochs
 
     def fit(self, X, y):
         
@@ -20,25 +27,31 @@ class LinearRegression:
         
         # batch gradient descent
         if self.solver  == 'bgd':  
-            theta = np.random.randn(2, 1) 
+            theta = np.random.randn(X_b.shape[1], 1)
 
             m = len(X)
             for iteration in range(self.n_iterations):
-                gradient = 2 / m * X_b.T @ (X_b @ theta - y)
+                gradient = 2 / m * X_b.T @ (X_b @ theta - y_b)
                 theta = theta - gradient * self.learning_rate
 
             return theta
 
+        # stochastic gradient descent
+        if self.solver == 'sgd':
+            theta = np.random.randn(X_b.shape[1], 1)
 
+            m = len(X)
+            for epoch in range(self.epochs):
+                for i in range(m):
+                    random_index = np.random.randint(100)
 
+                    Xi = X_b[random_index:random_index+1]
+                    yi = y_b[random_index:random_index+1]
 
-
-
-        
-X = np.random.randint(1, 100, size=100)
-y = 4 * X
-
-LinearRegression(solver='ne', learning_rate=0.05, n_iterations=100).fit(X, y)
+                    gradients = 2 * Xi.T.dot(Xi.dot(theta) - yi)
+                    theta = theta - gradients * self.learning_rate
+            
+            return theta
 
         
     
